e0558995b5afe7a368d749e0c48af245075ca029
Kuan Jiang <Kuan Jiang>
2017-04-12 22:03:39 -0400

    fix try 2

--- poll-app/src/app/app.component.spec.ts
+++ poll-app/src/app/app.component.spec.ts
@@ -28,14 +28,14 @@ describe('Component: App', () => {
   it('should display the navigation bar correctly', () => {
     const de = fixture.debugElement.queryAll(By.css('a'));
     expect(de.length).toBe(4);
-    expect(de[0].nativeElement.textContent).toContain('Students');
+    expect(de[0].nativeElement.textContent).toContain('Student');
     expect(de[1].nativeElement.textContent).toContain('Stats');
 	expect(de[2].nativeElement.textContent).toContain('About');
 	expect(de[3].nativeElement.textContent).toContain('Teacher');
     expect(de[0].attributes['routerLink']).toBe('/');
     expect(de[1].attributes['routerLink']).toBe('/stats');
     expect(de[2].attributes['routerLink']).toBe('/about');
-    expect(de[3].attributes['routerLink']).toBe('/teacher');
+    expect(de[3].attributes['routerLink']).toBe('/auth');
   });
 
 });
