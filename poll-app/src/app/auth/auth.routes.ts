567f5a0a11d0d366da2ec78a2422b0db74f050e8
Karlo Gonzales <karlo.gonzales@gmail.com>
2017-04-18 01:41:11 -0400

    remove signin/signup

--- poll-app/src/app/auth/auth.routes.ts
+++ poll-app/src/app/auth/auth.routes.ts
@@ -5,7 +5,7 @@ import { SigninComponent } from "./signin.component";
 import { LogoutComponent } from "./logout.component";
 
 export const AUTH_ROUTES: Routes = [
-    { path: '', redirectTo: 'signup', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'signup', component: SignupComponent },
     { path: 'signin', component: SigninComponent },
     { path: 'logout', component: LogoutComponent }
--- poll-app/src/app/auth/authentication.component.ts
+++ poll-app/src/app/auth/authentication.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from "./auth.service";
         <header class="row spacing">
             <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-tabs">
-                    <li routerLinkActive="active"><a [routerLink]="['signup']">Signup</a></li>
+                    <li routerLinkActive="active" *ngIf="!isLoggedIn()"><a [routerLink]="['signup']">Signup</a></li>
                     <li routerLinkActive="active" *ngIf="!isLoggedIn()"><a [routerLink]="['signin']">Signin</a></li>
                     <li routerLinkActive="active" *ngIf="isLoggedIn()"><a [routerLink]="['logout']">Logout</a></li>
                 </ul>
